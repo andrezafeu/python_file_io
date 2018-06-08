8720548d0e9e63cd44fb07e3b8b59b02ac839efd
Andreza Feu <andrezafeu@gmail.com>
2018-06-08 11:08:06 -0400

    Refactoring

--- remember.py
+++ remember.py
@@ -1,9 +1,8 @@
 def remember(thing):
     # by default, open() opens a file for reading. ex: file = open('database.txt')
     # 'a' open for writing, appending to the end of the file
-    file = open('database.txt', 'a')
-    file.write(thing+"\n")
-    # file.close() is being used just to be more explicit. When the function ends, things inside would've been garbage collected anyway.
-    file.close()
+    with open('database.txt', 'a') as file:
+        file.write(thing+"\n")
+
 
 remember(input("What should I remember? "))
