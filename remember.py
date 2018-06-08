a3dcd9acac2704a9b9a348e3ac9ad741da1c54bb
Andreza Feu <andrezafeu@gmail.com>
2018-06-08 15:31:36 -0400

    Add function to print list

--- remember.py
+++ remember.py
@@ -1,8 +1,21 @@
+import sys
+
 def remember(thing):
-    # by default, open() opens a file for reading. ex: file = open('database.txt')
     # 'a' open for writing, appending to the end of the file
     with open('database.txt', 'a') as file:
         file.write(thing+"\n")
 
 
-remember(input("What should I remember? "))
+def show():
+    with open('database.txt') as file:
+        for line in file:
+            print(line)
+
+
+# https://stackoverflow.com/questions/419163/what-does-if-name-main-do
+if __name__ == '__main__':
+    # sys.argv lists all arguments
+    if sys.argv[1].lower() == '--list':
+        show()
+    else:
+        remember(' '.join(sys.argv[1:]))
